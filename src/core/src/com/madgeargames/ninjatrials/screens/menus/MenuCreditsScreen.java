a79743e31006f5821f1c48eec2281c4ba341fd8a
bralmu <bralmu@gmail.com>
2016-05-23 17:53:31 +0200

    adds a background to the credits screen

--- src/core/src/com/madgeargames/ninjatrials/screens/menus/MenuCreditsScreen.java
+++ src/core/src/com/madgeargames/ninjatrials/screens/menus/MenuCreditsScreen.java
@@ -1,5 +1,12 @@
 package com.madgeargames.ninjatrials.screens.menus;
 
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas.AtlasRegion;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Interpolation;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.esotericsoftware.tablelayout.BaseTableLayout;
@@ -22,6 +29,23 @@ public class MenuCreditsScreen extends BaseScreen {
 	private Controller controller2;
 
 	public MenuCreditsScreen() {
+		ImgActor bg0 = new ImgActor(Assets.menuVarious.menu_main_bg);
+		bg0.setColor(1f, 1f, 1f, .5f);
+		bg0.setRotation(315f);
+		bg0.scaleBy(1.33f);
+		bg0.addAction(Actions
+				.forever(Actions.sequence(Actions.alpha(.3f, 1f, Interpolation.linear), Actions.alpha(.7f, 1f))));
+		bg0.addAction(Actions.forever(Actions.rotateBy(2f, 1f)));
+		stage.addActor(bg0);
+		ImgActor bg1 = new ImgActor(Assets.menuVarious.menu_main_bg);
+		bg1.setColor(1f, 1f, 1f, .5f);
+		bg1.setRotation(45);
+		bg1.scaleBy(1.2f);
+		bg1.addAction(Actions
+				.forever(Actions.sequence(Actions.alpha(.6f, .83f, Interpolation.linear), Actions.alpha(.4f, .79f))));
+		bg1.addAction(Actions.forever(Actions.rotateBy(-.5f, 1f)));
+		stage.addActor(bg1);
+
 		table = new Table();
 		table.setFillParent(true);
 		table.defaults().spaceBottom(30 * Constants.R);
@@ -95,6 +119,41 @@ public class MenuCreditsScreen extends BaseScreen {
 		@Override
 		public void onPressEsc() {
 			exit();
+		}
+	}
+
+	public class ImgActor extends Actor {
+		public AtlasRegion region;
+		public float offsetX;
+		public float offsetY;
+
+		public ImgActor(TextureRegion textureRegion) {
+			region = (AtlasRegion) textureRegion;
+			offsetX = region.offsetX;
+			offsetY = region.offsetY;
+			setX(offsetX);
+			setY(offsetY);
+			setOriginX(region.getRegionWidth() / 2);
+			setOriginY(region.getRegionHeight() / 2);
+			setWidth(region.getRegionWidth());
+			setHeight(region.getRegionHeight());
+			setScaleX(1f);
+			setScaleY(1f);
+			setRotation(0f);
+		}
+
+		@Override
+		public void act(float delta) {
+			super.act(delta);
+		}
+
+		@Override
+		public void draw(Batch batch, float parentAlpha) {
+			Color color = getColor();
+			batch.setColor(color.r, color.g, color.b, color.a * parentAlpha);
+			batch.draw(region, getX(), getY(), getOriginX(), getOriginY(), getWidth(), getHeight(), getScaleX(),
+					getScaleY(), getRotation());
+			batch.setColor(1f, 1f, 1f, 1f);
 		}
 	}
 
